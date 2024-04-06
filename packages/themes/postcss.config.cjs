a9457515d019ff762ac11cd9293cedba932fd8ed
pimenovoleg <pimenov.o@gmail.com>
2024-04-06 21:34:24 +0300

    chore: added theme

---
+++ packages/themes/postcss.config.cjs
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        require('postcss-import')({
+            path: [path.relative(process.cwd(), '../')],
+        }),
+        require('postcss-nesting'),
+        require('./postcss-breakpoints.cjs'),
+        require('postcss-custom-media'),
+        require('postcss-combine-duplicated-selectors'),
+        require('postcss-discard-empty'),
+        require('./postcss-whitespace.cjs'),
+        require('autoprefixer'),
+    ],
+};
