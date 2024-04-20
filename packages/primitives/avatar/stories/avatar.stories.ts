7503f82ef252e4a3e2b9e88d72eb3dd13a6a6f70
Oleg Pimenov <pimenov.o@gmail.com>
2024-04-20 11:30:08 +0300

    chore(avatar): upd stories

---
+++ packages/primitives/avatar/stories/avatar.stories.ts
@@ -0,0 +1,88 @@
+import { componentWrapperDecorator, Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+
+import { RdxAvatarFallbackDirective } from '../src/avatar-fallback.directive';
+import { RdxAvatarImageDirective } from '../src/avatar-image.directive';
+import { RdxAvatarDirective } from '../src/avatar.directive';
+
+export default {
+    title: 'Primitives/Avatar',
+    decorators: [
+        moduleMetadata({
+            imports: [RdxAvatarDirective, RdxAvatarImageDirective, RdxAvatarFallbackDirective]
+        }),
+        componentWrapperDecorator(
+            (story) =>
+                `<div class="radix-themes light light-theme radix-themes-default-fonts"
+                      data-accent-color="indigo"
+                      data-radius="medium"
+                      data-scaling="100%">${story}</div>`
+        )
+    ]
+} as Meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+    render: () => ({
+        template: `
+<div style=" display: flex; gap: 20px">
+    <span rdxAvatar class="AvatarRoot">
+      <img rdxAvatarImage
+            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+            alt="Colm Tuite"
+            class="AvatarImage" />
+      <span rdxAvatarFallback class="AvatarFallback" rdxAvatarFallbackDelay="600">CT</span>
+    </span>
+
+    <span rdxAvatar class="AvatarRoot">
+          <img rdxAvatarImage
+            class="AvatarImage"
+            src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+            alt="Pedro Duarte"
+          />
+          <span rdxAvatarFallback class="AvatarFallback" rdxAvatarFallbackDelay="600">
+            JD
+          </span>
+        </span>
+    <span rdxAvatar class="AvatarRoot">
+      <span rdxAvatarFallback class="AvatarFallback">PD</span>
+    </span>
+</div>
+<style>
+.AvatarRoot {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+  overflow: hidden;
+  user-select: none;
+  width: 45px;
+  height: 45px;
+  border-radius: 100%;
+  background-color: var(--black-a3);
+}
+
+.AvatarImage {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+}
+
+.AvatarFallback {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  color: var(--violet-11);
+  font-size: 15px;
+  line-height: 1;
+  font-weight: 500;
+}
+
+</style>
+`
+    })
+};
