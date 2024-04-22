fa07716b8a31f693a2939663ba419ca480d67b70
Oleg Pimenov <pimenov.o@gmail.com>
2024-04-22 08:40:17 +0300

    chore(taxonomy): added separator component

--- apps/showcase-taxonomy/src/app/components/ui/index.ts
+++ apps/showcase-taxonomy/src/app/components/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './button.component';
 export * from './label.component';
 export * from './input.component';
+export * from './separator.component';
---
+++ apps/showcase-taxonomy/src/app/components/ui/separator.component.ts
@@ -0,0 +1,32 @@
+import { computed, Directive, Input, input } from '@angular/core';
+
+import { SeparatorDirective as RadixSeparator } from '@radix-ng/primitives/separator';
+import { twMerge } from '@taxonomy/components/utils/twMerge';
+import { ClassValue } from 'clsx';
+
+@Directive({
+    selector: '[txSeparator]',
+    standalone: true,
+    hostDirectives: [
+        {
+            directive: RadixSeparator,
+            inputs: ['rdxSeparatorOrientation: txOrientation', 'rdxSeparatorDecorative']
+        }
+    ],
+    host: {
+        '[class]': '_computedClass()'
+    }
+})
+export class TxSeparatorDirective {
+    @Input('txOrientation') orientation = 'horizontal';
+
+    public readonly userClass = input<ClassValue>('', { alias: 'class' });
+    protected readonly _computedClass = () =>
+        computed(() =>
+            twMerge(
+                'shrink-0 bg-border',
+                `${this.orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]'}`,
+                this.userClass()
+            )
+        );
+}
