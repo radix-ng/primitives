058155de027c6452a25509fac779cbd2192e56a1
Oleg Pimenov <pimenov.o@gmail.com>
2024-05-09 20:20:22 +0300

    chore: added wip primitives

---
+++ packages/primitives/button/src/button-abstract.directive.ts
@@ -0,0 +1,50 @@
+import {
+    booleanAttribute,
+    Directive,
+    ElementRef,
+    EventEmitter,
+    inject,
+    Input,
+    Output
+} from '@angular/core';
+
+type ButtonType = 'button' | 'submit' | 'reset';
+
+let idIterator = 0;
+
+@Directive({
+    standalone: true,
+    host: {
+        '[attr.id]': 'this.id',
+        '[attr.role]': 'this.type',
+        '[attr.disabled]': 'disabled ? disabled : null',
+        '(click)': 'onClick($event)',
+        '(blur)': 'onBlur()'
+    }
+})
+export abstract class RdxButtonAbstractDirective {
+    #elementRef = inject(ElementRef);
+
+    @Input({ transform: booleanAttribute }) disabled = false;
+
+    @Input() type: ButtonType = 'button';
+
+    @Output() onClickHandler = new EventEmitter<MouseEvent>();
+
+    @Input() id = `rdx-button-${idIterator++}`;
+
+    protected focused = false;
+
+    protected onClick(event: MouseEvent) {
+        this.onClickHandler.emit(event);
+        this.focused = false;
+    }
+
+    protected onBlur() {
+        this.focused = false;
+    }
+
+    get nativeElement() {
+        return this.#elementRef.nativeElement;
+    }
+}
--- packages/primitives/label/src/label-root.directive.ts
+++ packages/primitives/label/src/label-root.directive.ts
@@ -1,15 +1,20 @@
-import { Directive, ElementRef, inject, input } from '@angular/core';
+import { Directive, ElementRef, inject, Input, input } from '@angular/core';
+
+let idIterator = 0;
 
 @Directive({
     selector: 'label[LabelRoot]',
     exportAs: 'LabelRoot',
     standalone: true,
     host: {
+        '[attr.id]': 'this.id',
         '[attr.for]': 'htmlFor ? htmlFor() : null',
         '(mousedown)': 'onMouseDown($event)'
     }
 })
 export class RdxLabelRootDirective {
+    @Input() id = `rdx-label-${idIterator++}`;
+
     /**
      * The id of the element the label is associated with.
      * @default '-'
