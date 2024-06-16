0b6cc905a5886a00dccf92350a6f782d42a71f47
Vitaliy Agoshkov <fracturer@gmail.com>
2024-06-16 20:50:47 +0500

    feat: add sb story

--- packages/primitives/collapsible/stories/collapsible.stories.ts
+++ packages/primitives/collapsible/stories/collapsible.stories.ts
@@ -19,7 +19,54 @@ export default {
                 <div class="radix-themes light light-theme radix-themes-default-fonts"
                     data-accent-color="indigo"
                     data-radius="medium"
-                    data-scaling="100%">${story}</div>`
+                    data-scaling="100%"
+                >
+                    ${story}
+                </div>
+
+                <style>
+                    .CollapsibleRoot {
+                        width: 300px;
+
+                    }
+
+                    .IconButton {
+                        font-family: inherit;
+                        border-radius: 100%;
+                        height: 25px;
+                        width: 25px;
+                        display: inline-flex;
+                        align-items: center;
+                        justify-content: center;
+                        color: var(--violet-11);
+                        box-shadow: 0 2px 10px var(--black-a7);
+                    }
+
+                    .Text {
+                        color: var(--violet-11);
+                        font-size: 15px;
+                        line-height: 25px;
+                    }
+
+                    .Repository {
+                        background-color: white;
+                        border-radius: 4px;
+                        margin: 10px 0;
+                        padding: 10px;
+                        box-shadow: 0 2px 10px var(--black-a7);
+                    }
+
+                    .collapse-content {
+                        overflow: hidden;
+                        transition: height 0.5s ease-in-out;
+                    }
+                    .collapse:not(.show) {
+                        display: none;
+                    }
+                    .show {
+                        height: auto;
+                    }
+                </style>`
         )
     ]
 } as Meta;
@@ -29,12 +76,25 @@ type Story = StoryObj;
 export const Default: Story = {
     render: () => ({
         template: `
-            <div CollapsibleRoot [open]="true">
-                <button CollapsibleTrigger>trigger</button>
+            <div class="CollapsibleRoot" CollapsibleRoot [open]="true">
 
-                <div>Some Content</div>
+                <div style="display: flex; align-items: center; justify-content: space-between;">
+                <span class="Text" style="color: white">&#64;peduarte starred 3 repositories</span>
+                <button class="IconButton" CollapsibleTrigger></button>
+                </div>
 
-                <div CollapsibleContent>Collapsible Content</div>
+                <div class="Repository">
+                    <span class="Text">&#64;radix-ui/primitives</span>
+                </div>
+
+                <div CollapsibleContent class="collapse-content">
+                    <div class="Repository">
+                        <span class="Text">&#64;radix-ui/colors</span>
+                    </div>
+                    <div class="Repository">
+                        <span class="Text">&#64;stitches/react</span>
+                    </div>
+                </div>
             </div>
         `
     })
