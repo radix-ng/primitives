1c15d08b10bec0e6de0c45d45204c1df6f06e9e0
Vitaliy Agoshkov <fracturer@gmail.com>
2024-06-17 21:42:15 +0500

    feat: fix tests

--- packages/primitives/collapsible/__tests__/collapsible-trigger.directive.spec.ts
+++ packages/primitives/collapsible/__tests__/collapsible-trigger.directive.spec.ts
@@ -1,8 +1,31 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RdxCollapsibleRootDirective } from '../src/collapsible-root.directive';
 import { RdxCollapsibleTriggerDirective } from '../src/collapsible-trigger.directive';
 
+@Component({
+    selector: 'rdx-collapsible-mock-trigger',
+    standalone: true,
+    imports: [RdxCollapsibleRootDirective, RdxCollapsibleTriggerDirective],
+    template: `
+        <div CollapsibleRoot>
+            <button CollapsibleTrigger>Trigger</button>
+        </div>
+    `
+})
+class RdxCollapsibleMockComponent {}
+
 describe('RdxCollapsibleTriggerDirective', () => {
+    let component: RdxCollapsibleMockComponent;
+    let fixture: ComponentFixture<RdxCollapsibleMockComponent>;
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RdxCollapsibleMockComponent);
+        component = fixture.componentInstance;
+    });
+
     it('should create an instance', () => {
-        const directive = new RdxCollapsibleTriggerDirective();
-        expect(directive).toBeTruthy();
+        expect(component).toBeTruthy();
     });
 });
