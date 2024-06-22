5005806cfd27c00b9d8229161f369848267f8378
Oleg Pimenov <pimenov.o@gmail.com>
2024-06-22 17:24:10 +0300

    chore: remove docs app

--- apps/docs/src/app/shared/header/header.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'radix-ng-docs-header',
-    standalone: true,
-    templateUrl: 'header.component.html'
-})
-export class HeaderComponent {}
--- apps/docs/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import 'zone.js';
-
-import { appConfig } from './app.config';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
