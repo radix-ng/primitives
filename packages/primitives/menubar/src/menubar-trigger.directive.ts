9541d2c82f646a18186316e81482f0093a785df7
Oleg Pimenov <pimenov.o@gmail.com>
2024-06-29 09:51:14 +0300

    chore: upd

--- packages/primitives/menubar/src/menubar-trigger.directive.ts
+++ packages/primitives/menubar/src/menubar-trigger.directive.ts
@@ -1,5 +1,6 @@
+import { BooleanInput } from '@angular/cdk/coercion';
 import { CdkMenuTrigger } from '@angular/cdk/menu';
-import { booleanAttribute, Directive, Input } from '@angular/core';
+import { booleanAttribute, Directive, input, Input } from '@angular/core';
 
 @Directive({
     selector: '[MenuBarTrigger]',
@@ -9,14 +10,16 @@ import { booleanAttribute, Directive, Input } from '@angular/core';
         type: 'button',
         role: 'menuitem',
         '[attr.aria-haspopup]': "'menu'",
-        '[attr.data-disabled]': "disabled ? '' : undefined",
-        '[disabled]': 'disabled',
+        '[attr.data-disabled]': "disabled() ? '' : undefined",
+        '[disabled]': 'disabled()',
 
         '(onpointerdown)': 'onPointerDown($event)'
     }
 })
 export class RdxMenuBarTriggerDirective {
-    @Input({ transform: booleanAttribute }) disabled = false;
+    readonly disabled = input<boolean, BooleanInput>(false, {
+        transform: booleanAttribute
+    });
 
     onPointerDown($event: MouseEvent) {
         if (!this.disabled && $event.button === 0 && !$event.ctrlKey) {
