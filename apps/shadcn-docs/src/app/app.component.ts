5da21128e817a32ab3c0e9ca86772c5790855c33
Oleg Pimenov <pimenov.o@gmail.com>
2024-07-01 15:25:00 +0300

    chore(shadcn): upd doc theme

--- apps/shadcn-docs/src/app/app.component.ts
+++ apps/shadcn-docs/src/app/app.component.ts
@@ -38,7 +38,38 @@ import { ThemeSwitcherComponent } from './ui/theme-switcher/theme-switcher.compo
                 [rightContent]="rightContent"
             >
                 <ng-template #leftContent>
-                    <h3 style="margin: 0">shadcn/ui</h3>
+                    <a class="mr-6 flex items-center space-x-2" href="/">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 256 256"
+                            class="h-6 w-6"
+                        >
+                            <rect width="256" height="256" fill="none"></rect>
+                            <line
+                                x1="208"
+                                y1="128"
+                                x2="128"
+                                y2="208"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="16"
+                            ></line>
+                            <line
+                                x1="192"
+                                y1="40"
+                                x2="40"
+                                y2="192"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="16"
+                            ></line>
+                        </svg>
+                        <span class="hidden font-bold sm:inline-block">shadcn/ui</span>
+                    </a>
                 </ng-template>
                 <ng-template #rightContent>
                     <div class="flex gap-4">
