d463f1eaabaf7472d66d5ca074ee6bef2a7ef8c9
Oleg Pimenov <pimenov.o@gmail.com>
2024-07-02 10:44:29 +0300

    Revert "fix(shadcn): tailwind preset plugin"
    This reverts commit 906de5855670d77eb090b8f10e175c65dea7857f.

---
+++ apps/storybook-radix/.storybook/manager.ts
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/manager-api';
+
+import rdxTheme from './rdxTheme';
+
+addons.setConfig({
+    theme: rdxTheme
+});
---
+++ apps/storybook-radix/.storybook/rdxTheme.ts
@@ -0,0 +1,24 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Radix Angular',
+    brandUrl: 'https://github.com/radix-ng/primitives',
+    brandTarget: '_self',
+
+    colorPrimary: '#272E35',
+    colorSecondary: '#272E35',
+
+    appBg: '#ffffff',
+    appContentBg: '#ffffff',
+    appBorderColor: '#EAEDF0',
+    appBorderRadius: 8,
+
+    textColor: '#272E35',
+    textInverseColor: '#ffffff',
+
+    inputBg: '#ffffff',
+    inputBorder: '#CFD6DD',
+    inputTextColor: '#272E35',
+    inputBorderRadius: 8
+});
