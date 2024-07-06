9761dbfb29f5fd1d06fd1058a342ad8abfb5c9b1
zizifn <1803942+zizifn@users.noreply.github.com>
2024-07-06 23:50:28 +0800

    chore(alert-dialog): fix a11y keyboard issue for AlertDialog (#47)

--- packages/primitives/alert-dialog/src/alert-dialog.service.ts
+++ packages/primitives/alert-dialog/src/alert-dialog.service.ts
@@ -41,6 +41,11 @@ export class AlertDialogService {
         );
         this.overlayRef.attach(templatePortal);
 
+        this.overlayRef.keydownEvents().subscribe((event) => {
+            if (event.key === 'Escape' || event.code === 'Escape') {
+                this.close();
+            }
+        });
         this.overlayRef.backdropClick().subscribe(() => this.close());
     }
 
