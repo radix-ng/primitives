6c24ef84594b7607c78d513100027b76256111b7
Oleg Pimenov <pimenov.o@gmail.com>
2024-07-07 22:09:31 +0300

    chore(shadcn): docs – Themes

--- apps/shadcn-docs/src/app/docs/docs.component.ts
+++ apps/shadcn-docs/src/app/docs/docs.component.ts
@@ -50,7 +50,8 @@ export class DocsComponent {
     }[] = [
         { path: '/docs/getting-started/installation', exact: true, label: 'Docs' },
         { path: '/docs/components/badge', exact: true, label: 'Components' },
-        { path: 'https://blocks.shadcn-ng.com/', external: true, label: 'Blocks' }
+        { path: 'https://blocks.shadcn-ng.com/', external: true, label: 'Blocks' },
+        { path: '/themes', exact: true, label: 'Themes' }
     ];
 
     isActive(route: { path: string; exact?: boolean; label: string; external?: boolean }): boolean {
