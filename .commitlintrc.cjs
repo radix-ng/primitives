c6de79dd32e2241a4cbfd5448e5c645b89c5c561
Oleg Pimenov <pimenov.o@gmail.com>
2024-07-08 23:26:20 +0300

    chore: upd commitlint rules

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,3 +1,20 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'primitives',
+                'shadcn',
+                'docs',
+                'release',
+                'deps',
+                'deps-dev',
+                'changelog',
+                'ci',
+                'build'
+            ]
+        ]
+    }
 };
