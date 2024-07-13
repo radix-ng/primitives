8150f40fa1ebbdeb5eb9eebeb9c221ff268822c7
Oleg Pimenov <pimenov.o@gmail.com>
2024-07-13 15:10:42 +0300

    feat(shadcn): added part of typography

---
+++ packages/shadcn/typography/index.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+import {
+    ShTypographyH1Directive,
+    ShTypographyH2Directive,
+    ShTypographyListDirective
+} from './src/typography.directive';
+
+export * from './src/typography.directive';
+
+const importsTypography = [
+    ShTypographyH1Directive,
+    ShTypographyH2Directive,
+    ShTypographyListDirective
+];
+@NgModule({
+    imports: [...importsTypography],
+    exports: [...importsTypography]
+})
+export class ShTypographyModule {}
