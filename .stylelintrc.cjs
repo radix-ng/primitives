2736f8d78276b5a2211682244412b51d545a9000
Artem Belik <artyombelyk@gmail.com>
2024-07-26 21:12:20 +0300

    feat: added stylelint support (#64)

---
+++ .stylelintrc.cjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+/**
+ * @type {import('stylelint').Config}
+ */
+const config = {
+    defaultSeverity: 'error',
+    allowEmptyInput: true,
+    extends: [
+        'stylelint-config-recommended-scss',
+        // should be last
+        'stylelint-prettier/recommended'
+    ],
+    rules: {
+        'rule-empty-line-before': [
+            'always-multi-line',
+            {
+                except: ['first-nested'],
+                ignore: ['after-comment']
+            }
+        ],
+        'scss/at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: ['tailwind', 'apply']
+            }
+        ],
+        'no-descending-specificity': null,
+        'scss/operator-no-unspaced': null
+    }
+};
+
+module.exports = config;
