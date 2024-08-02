85c203da2c13c57aabd476a44be09fc7ba2995b2
lskramarov <l.s.kramarov@gmail.com>
2024-08-02 16:22:09 +0300

    fix: rdxChecked

--- packages/primitives/dropdown-menu/src/dropdown-menu-item-selectable.ts
+++ packages/primitives/dropdown-menu/src/dropdown-menu-item-selectable.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, booleanAttribute } from '@angular/core';
+import { Directive, Input, booleanAttribute, Output, EventEmitter } from '@angular/core';
 import { RdxDropdownMenuItemDirective } from '@radix-ng/primitives/dropdown-menu';
 
 
@@ -6,13 +6,29 @@ import { RdxDropdownMenuItemDirective } from '@radix-ng/primitives/dropdown-menu
 @Directive({
     host: {
         '[attr.aria-checked]': '!!checked',
-        '[attr.aria-disabled]': 'disabled || null'
+        '[attr.aria-disabled]': 'disabled || null',
+        '[attr.data-state]': 'checked ? "checked" : "unchecked"'
     },
     standalone: true
 })
 export abstract class RdxDropdownMenuSelectable extends RdxDropdownMenuItemDirective {
     /** Whether the element is checked */
-    @Input({ alias: 'rdxDropdownMenuItemChecked', transform: booleanAttribute }) checked: boolean = false;
+    @Input({ alias: 'rdxChecked', transform: booleanAttribute })
+    set checked(value: boolean) {
+        if (this._checked == value) return;
+
+        this._checked = value;
+
+        this.rdxCheckedChange.emit(value);
+    }
+
+    get checked() {
+        return this._checked;
+    }
+
+    private _checked = false
+
+    @Output() readonly rdxCheckedChange = new EventEmitter<boolean>();
 
     /** Whether the item should close the menu if triggered by the spacebar. */
     protected override closeOnSpacebarTrigger = false;
