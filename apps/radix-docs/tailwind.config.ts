df623b5ee3442a1c7443cb4f8fe1e78e823d718b
Oleg Pimenov <pimenov.o@gmail.com>
2024-08-04 23:36:30 +0300

    chore: upd astro check

--- apps/radix-docs/tailwind.config.ts
+++ apps/radix-docs/tailwind.config.ts
@@ -1,5 +1,5 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
 import { type Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
