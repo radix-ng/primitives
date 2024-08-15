1866723a77e0a66b583f3d7b68bd8adbaf3c8264
Oleg Pimenov <pimenov.o@gmail.com>
2024-08-15 16:36:14 +0300

    chore(shadcn): updated example dropdown-menu

--- apps/shadcn-docs/src/content/components/dropdown-menu/examples/dropdown-example-checkbox.component.ts
+++ apps/shadcn-docs/src/content/components/dropdown-menu/examples/dropdown-example-checkbox.component.ts
@@ -1,37 +1,21 @@
-import { CdkMenu } from '@angular/cdk/menu';
 import { Component } from '@angular/core';
-import {
-    RdxDropdownMenuItemIndicatorDirective,
-    RdxDropdownMenuTriggerDirective
-} from '@radix-ng/primitives/dropdown-menu';
-import { RdxMenuBarDirective } from '@radix-ng/primitives/menubar';
+import { RdxDropdownMenuTriggerDirective } from '@radix-ng/primitives/dropdown-menu';
 import { ShButtonDirective } from '@radix-ng/shadcn/button';
 import {
     ShDropdownMenuCheckboxItemComponent,
-    ShDropdownMenuCheckboxItemDirective,
     ShDropdownMenuContent,
-    ShDropdownMenuItem,
     ShDropdownMenuLabel,
-    ShDropdownMenuSeparator,
-    ShDropdownMenuShortcut
+    ShDropdownMenuSeparator
 } from '@radix-ng/shadcn/dropdown-menu';
-import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
     standalone: true,
     imports: [
-        RdxMenuBarDirective,
         RdxDropdownMenuTriggerDirective,
         ShButtonDirective,
         ShDropdownMenuContent,
         ShDropdownMenuSeparator,
         ShDropdownMenuLabel,
-        ShDropdownMenuItem,
-        ShDropdownMenuShortcut,
-        ShDropdownMenuCheckboxItemDirective,
-        CdkMenu,
-        LucideAngularModule,
-        RdxDropdownMenuItemIndicatorDirective,
         ShDropdownMenuCheckboxItemComponent
     ],
     template: `
@@ -41,18 +25,12 @@ import { LucideAngularModule } from 'lucide-angular';
             <div class="w-56" shDropdownMenuContent>
                 <div shDropdownMenuLabel>Appearance</div>
                 <div shDropdownMenuSeparator></div>
-                <button
+                <sh-dropdown-menu-checkbox-item
                     [(checked)]="showStatusBar"
                     (checkedChange)="showStatusBarChange($event)"
-                    shDropdownMenuCheckboxItem
                 >
-                    <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-                        <div rdxDropdownMenuItemIndicator>
-                            <lucide-icon size="16" name="check"></lucide-icon>
-                        </div>
-                    </span>
                     Status Bar
-                </button>
+                </sh-dropdown-menu-checkbox-item>
                 <sh-dropdown-menu-checkbox-item [(checked)]="showActivityBar">
                     Activity Bar
                 </sh-dropdown-menu-checkbox-item>
