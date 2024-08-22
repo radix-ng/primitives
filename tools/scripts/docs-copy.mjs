6a1ca7eb893d57693f29be0c35fcf33d199dd2b2
Oleg Pimenov <pimenov.o@gmail.com>
2024-08-22 12:26:04 +0300

    Revert "chore: scripts for vercel"
    This reverts commit e767409b5fdbf4e0564b6d5e06d1c6778ad068a9.

--- tools/scripts/docs-copy.mjs
+++
@@ -1,5 +0,0 @@
-import { cpSync, mkdirSync } from 'fs';
-
-mkdirSync('apps/radix-docs/node_modules/@radix-ng/primitives', { recursive: true });
-
-cpSync('dist/primitives', 'apps/radix-docs/node_modules/@radix-ng', { recursive: true });
