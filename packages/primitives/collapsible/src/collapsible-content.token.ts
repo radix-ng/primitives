24baf805415c8b2055c0daf9ea3e15f7ae368efb
Oleg Pimenov <pimenov.o@gmail.com>
2024-08-29 00:05:16 +0300

    feat(collapsible): rename onOpenChange and added content attributes

---
+++ packages/primitives/collapsible/src/collapsible-content.token.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from '@angular/core';
+import { RdxCollapsibleContentDirective } from './collapsible-content.directive';
+
+export const RdxCollapsibleContentToken = new InjectionToken<RdxCollapsibleContentDirective>(
+    'RdxCollapsibleContentToken'
+);
