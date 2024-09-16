e5ebbc62009c9839245baef2213b0404f1e409ad
Oleg Pimenov <pimenov.o@gmail.com>
2024-09-16 15:33:29 +0300

    feat(radio): added prefix rdx

--- packages/primitives/radio/stories/radio.stories.ts
+++ packages/primitives/radio/stories/radio.stories.ts
@@ -5,6 +5,8 @@ import { RdxRadioItemDirective } from '../src/radio-item.directive';
 import { RdxRadioGroupDirective } from '../src/radio-root.directive';
 import { RadioGroupComponent } from './radio-group.component';
 
+const html = String.raw;
+
 export default {
     title: 'Primitives/Radio Group',
     decorators: [
@@ -30,104 +32,119 @@ type Story = StoryObj;
 
 export const Default: Story = {
     render: () => ({
-        template: `
-<form>
-    <div RadioRoot class="RadioGroupRoot" aria-label="View density">
-        <div style="display: flex; align-items: center;">
-            <button RadioItem class="RadioGroupItem" value="default" id="r1">
-                <div RadioIndicator class="RadioGroupIndicator"></div>
-                <input RadioIndicator type="radio" aria-hidden="true" tabindex="-1" value="default" class="Input">
-            </button>
-            <label rdxLabel htmlFor="r1" class="Label">
-              Default
-            </label>
-        </div>
-        <div style="display: flex; align-items: center;">
-            <button RadioItem class="RadioGroupItem" value="comfortable" id="r2">
-                 <div RadioIndicator class="RadioGroupIndicator"></div>
-                 <input RadioIndicator type="radio" aria-hidden="true" tabindex="-1" value="comfortable" class="Input">
-            </button>
-            <label rdxLabel htmlFor="r2" class="Label">
-              Comfortable
-            </label>
-        </div>
-        <div style="display: flex; align-items: center;">
-            <button RadioItem class="RadioGroupItem" value="compact" id="r3">
-                 <div RadioIndicator class="RadioGroupIndicator"></div>
-                 <input RadioIndicator type="radio" aria-hidden="true" tabindex="-1" value="compact" class="Input">
-            </button>
-            <label rdxLabel htmlFor="r3" class="Label">
-              Compact
-            </label>
-        </div>
-    </div>
-</form>
+        template: html`
+            <form>
+                <div class="RadioGroupRoot" rdxRadioRoot aria-label="View density">
+                    <div style="display: flex; align-items: center;">
+                        <button class="RadioGroupItem" id="r1" rdxRadioItem value="default">
+                            <div class="RadioGroupIndicator" rdxRadioIndicator></div>
+                            <input
+                                class="Input"
+                                rdxRadioIndicator
+                                type="radio"
+                                aria-hidden="true"
+                                tabindex="-1"
+                                value="default"
+                            />
+                        </button>
+                        <label class="Label" rdxLabel htmlFor="r1">Default</label>
+                    </div>
+                    <div style="display: flex; align-items: center;">
+                        <button class="RadioGroupItem" id="r2" rdxRadioItem value="comfortable">
+                            <div class="RadioGroupIndicator" rdxRadioIndicator></div>
+                            <input
+                                class="Input"
+                                rdxRadioIndicator
+                                type="radio"
+                                aria-hidden="true"
+                                tabindex="-1"
+                                value="comfortable"
+                            />
+                        </button>
+                        <label class="Label" rdxLabel htmlFor="r2">Comfortable</label>
+                    </div>
+                    <div style="display: flex; align-items: center;">
+                        <button class="RadioGroupItem" id="r3" rdxRadioItem value="compact">
+                            <div class="RadioGroupIndicator" rdxRadioIndicator></div>
+                            <input
+                                class="Input"
+                                rdxRadioIndicator
+                                type="radio"
+                                aria-hidden="true"
+                                tabindex="-1"
+                                value="compact"
+                            />
+                        </button>
+                        <label class="Label" rdxLabel htmlFor="r3">Compact</label>
+                    </div>
+                </div>
+            </form>
 
-<style>
-/* reset */
-button {
-  all: unset;
-}
+            <style>
+                /* reset */
+                button {
+                    all: unset;
+                }
 
-.RadioGroupRoot {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
+                .RadioGroupRoot {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 10px;
+                }
 
-.RadioGroupItem {
-  background-color: white;
-  width: 25px;
-  height: 25px;
-  border-radius: 100%;
-  box-shadow: 0 2px 10px var(--black-a7);
-}
-.RadioGroupItem:hover {
-  background-color: var(--violet-3);
-}
-.RadioGroupItem:focus {
-  box-shadow: 0 0 0 2px black;
-}
+                .RadioGroupItem {
+                    background-color: white;
+                    width: 25px;
+                    height: 25px;
+                    border-radius: 100%;
+                    box-shadow: 0 2px 10px var(--black-a7);
+                }
+                .RadioGroupItem:hover {
+                    background-color: var(--violet-3);
+                }
+                .RadioGroupItem:focus {
+                    box-shadow: 0 0 0 2px black;
+                }
 
-.RadioGroupIndicator {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  position: relative;
-}
-.RadioGroupIndicator::after {
-  content: '';
-  display: block;
-  width: 11px;
-  height: 11px;
-  border-radius: 50%;
-  background-color: var(--violet-11);
-}
+                .RadioGroupIndicator {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 100%;
+                    height: 100%;
+                    position: relative;
+                }
+                .RadioGroupIndicator::after {
+                    content: '';
+                    display: block;
+                    width: 11px;
+                    height: 11px;
+                    border-radius: 50%;
+                    background-color: var(--violet-11);
+                }
 
-.RadioGroupIndicator[data-state="unchecked"] {
- display: none;
-}
+                .RadioGroupIndicator[data-state='unchecked'] {
+                    display: none;
+                }
 
-.Input {
-    transform: translateX(-100%);
-    position: absolute;
-    pointer-events: none;
-    opacity: 0;
-    margin: 0;
-    width: 25px;
-    height: 25px;
-}
+                .Input {
+                    transform: translateX(-100%);
+                    position: absolute;
+                    pointer-events: none;
+                    opacity: 0;
+                    margin: 0;
+                    width: 25px;
+                    height: 25px;
+                }
 
-.Label {
-  color: white;
-  font-size: 15px;
-  line-height: 1;
-  padding-left: 15px;
-}
-</style>
-`
+                .Label {
+                    color: white;
+                    font-size: 15px;
+                    line-height: 1;
+                    padding-left: 15px;
+                }
+            </style>
+        `
     })
 };
 
