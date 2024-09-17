c50ce18b3ad754466a9bb782aa56b923afb3fbad
Oleg Pimenov <pimenov.o@gmail.com>
2024-09-17 17:18:52 +0300

    fix(avatar): supported SSR

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -28,6 +28,7 @@ const config = {
             [
                 'primitives',
                 'accordion',
+                'avatar',
                 'collapsible',
                 'dialog',
                 'radio',
