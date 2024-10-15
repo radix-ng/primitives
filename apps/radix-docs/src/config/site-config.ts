f461456573cbbeea564c330c767a2fc6c8b4e499
Artem Belik <artyombelyk@gmail.com>
2024-10-15 19:50:57 +0300

    feat: added algolia docsearch support

--- apps/radix-docs/src/config/site-config.ts
+++ apps/radix-docs/src/config/site-config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-    title: 'radix-gn',
+    title: 'radix-ng',
     url: 'https://radix-ng.com',
     description: 'Unstyled, accessible components for building high-quality design systems and web apps in Angular.',
     links: {
