1aeecade6f7581f55559886faa821ac03f16c768
Oleg Pimenov <pimenov.o@gmail.com>
2024-10-16 14:18:01 +0300

    chore(radix-docs): change bg for demo

--- apps/radix-docs/tailwind.config.ts
+++ apps/radix-docs/tailwind.config.ts
@@ -68,6 +68,7 @@ const config = {
                 ring: 'hsl(var(--ring))',
                 background: 'hsl(var(--background))',
                 foreground: 'hsl(var(--foreground))',
+                'primitive-demo': 'var(--bg-primitive-demo)',
                 primary: {
                     DEFAULT: 'hsl(var(--primary))',
                     foreground: 'hsl(var(--primary-foreground))'
