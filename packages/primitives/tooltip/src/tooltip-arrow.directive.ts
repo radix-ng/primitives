483cdbdc13df7d2861bae738e4e73033fe4c05e8
Vitaliy Agoshkov <fracturer@gmail.com>
2024-10-16 09:29:35 +0500

    feat(primitives): arrow blank

--- packages/primitives/tooltip/src/tooltip-arrow.directive.ts
+++ packages/primitives/tooltip/src/tooltip-arrow.directive.ts
@@ -1,7 +1,34 @@
-import { Directive } from '@angular/core';
+import { computed, Directive, effect, ElementRef, inject, input, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[rdxTooltipArrow]',
     standalone: true
 })
-export class RdxTooltipArrowDirective {}
+export class RdxTooltipArrowDirective {
+    private readonly elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
+    private readonly renderer = inject(Renderer2);
+
+    width = input<number>(10);
+    height = input<number>(5);
+    arrowSvgElement = computed<HTMLElement>(() => {
+        const width = this.width();
+        const height = this.height();
+
+        const svgElement = this.renderer.createElement('svg', 'svg');
+        this.renderer.setAttribute(svgElement, 'width', `${width}`);
+        this.renderer.setAttribute(svgElement, 'height', `${height}`);
+        this.renderer.setAttribute(svgElement, 'viewBox', '0 0 30 10');
+        this.renderer.setAttribute(svgElement, 'fill', 'red');
+        const polygonElement = this.renderer.createElement('polygon', 'svg');
+        this.renderer.setAttribute(polygonElement, 'points', '0,0 30,0 15,10');
+        this.renderer.appendChild(svgElement, polygonElement);
+
+        return svgElement;
+    });
+
+    private readonly onArrowSvgElementChangeEffect = effect(() => {
+        const arrowElement = this.arrowSvgElement();
+
+        this.renderer.appendChild(this.elementRef.nativeElement, arrowElement);
+    });
+}
