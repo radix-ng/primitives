f8a1228242a148eb2eee784a2ae6747cf6a01815
Oleg Pimenov <pimenov.o@gmail.com>
2024-10-28 14:45:02 +0300

    feat(avatar): added role img

--- packages/primitives/avatar/src/avatar-image.directive.ts
+++ packages/primitives/avatar/src/avatar-image.directive.ts
@@ -10,6 +10,7 @@ export interface RdxAvatarImageProps {
     exportAs: 'rdxAvatarImage',
     standalone: true,
     host: {
+        role: 'img',
         '(load)': 'onLoad()',
         '(error)': 'onError()'
     }
