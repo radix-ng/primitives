e7b5f912720d193a8a320a39e3199ccfb210c067
Vitaliy Agoshkov <fracturer@gmail.com>
2024-11-22 09:00:41 +0500

    feat(primitives): tooltip content directive refactoring

--- packages/primitives/tooltip/src/tooltip-content.directive.ts
+++ packages/primitives/tooltip/src/tooltip-content.directive.ts
@@ -1,8 +1,7 @@
 import { ConnectedPosition } from '@angular/cdk/overlay';
 import { computed, Directive, forwardRef, inject, input, output, TemplateRef } from '@angular/core';
+import { getContentPosition } from './get-content-position';
 import { RdxTooltipContentToken } from './tooltip-content.token';
-import { RdxTooltipRootDirective } from './tooltip-root.directive';
-import { TOOLTIP_POSITIONS } from './tooltip.constants';
 import { RdxTooltipAlign, RdxTooltipSide } from './tooltip.types';
 
 @Directive({
@@ -12,52 +11,13 @@ import { RdxTooltipAlign, RdxTooltipSide } from './tooltip.types';
 })
 export class RdxTooltipContentDirective {
     readonly templateRef = inject(TemplateRef);
-    readonly tooltipRoot = inject(RdxTooltipRootDirective);
     readonly side = input<RdxTooltipSide>(RdxTooltipSide.Top);
     readonly sideOffset = input<number>(0);
     readonly align = input<RdxTooltipAlign>(RdxTooltipAlign.Center);
     readonly alignOffset = input<number>(0);
+    readonly position = computed<ConnectedPosition>(() =>
+        getContentPosition(this.side(), this.align(), this.sideOffset(), this.alignOffset())
+    );
     readonly onEscapeKeyDown = output<KeyboardEvent>();
     readonly onPointerDownOutside = output<MouseEvent>();
-
-    readonly position = computed<ConnectedPosition>(() => {
-        const side = this.side();
-        const align = this.align();
-        const sideOffset = this.sideOffset();
-        const alignOffset = this.alignOffset();
-
-        const position =
-            TOOLTIP_POSITIONS[side][align] ?? TOOLTIP_POSITIONS[RdxTooltipSide.Top][RdxTooltipAlign.Center];
-
-        if (sideOffset > 0) {
-            let xFactor = 0;
-            let yFactor = 0;
-
-            switch (side) {
-                case RdxTooltipSide.Top:
-                    yFactor = -1;
-                    break;
-                case RdxTooltipSide.Bottom:
-                    yFactor = 1;
-                    break;
-                case RdxTooltipSide.Left:
-                    xFactor = -1;
-                    break;
-                case RdxTooltipSide.Right:
-                    xFactor = 1;
-                    break;
-            }
-
-            position.offsetX = xFactor * sideOffset;
-            position.offsetY = yFactor * sideOffset;
-        }
-
-        if ([RdxTooltipAlign.Start, RdxTooltipAlign.End].includes(align) && alignOffset) {
-            const alignOffsetFactor = align === RdxTooltipAlign.End ? -1 : 1;
-
-            position.offsetX = alignOffsetFactor * alignOffset;
-        }
-
-        return position;
-    });
 }
