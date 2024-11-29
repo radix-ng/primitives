3360974f60e5c19c24930627989c5e575a92fb88
lskramarov <l.s.kramarov@gmail.com>
2024-11-29 16:32:38 +0300

    fix: test

--- packages/primitives/select/__tests__/select-root.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RdxSelectRootDirective } from '../src/select-root.directive';
-
-xdescribe('RdxSelectRootDirective', () => {
-    it('should create an instance', () => {
-        const directive = new RdxSelectRootDirective();
-        expect(directive).toBeTruthy();
-    });
-});
