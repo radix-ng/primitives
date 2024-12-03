84b034f3bc72131787ec8cbe1357633dee1b5c65
Vitaliy Agoshkov <fracturer@gmail.com>
2024-12-03 08:07:33 +0500

    feat(primitives): fix exports

--- packages/primitives/tooltip/index.ts
+++ packages/primitives/tooltip/index.ts
@@ -5,6 +5,11 @@ import { RdxTooltipContentDirective } from './src/tooltip-content.directive';
 import { RdxTooltipRootDirective } from './src/tooltip-root.directive';
 import { RdxTooltipTriggerDirective } from './src/tooltip-trigger.directive';
 
+export * from './src/tooltip-arrow.directive';
+export * from './src/tooltip-content-attributes.directive';
+export * from './src/tooltip-content.directive';
+export * from './src/tooltip-root.directive';
+export * from './src/tooltip-trigger.directive';
 export * from './src/tooltip.types';
 
 const directives = [
