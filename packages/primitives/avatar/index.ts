7e7c5f3f0d1fd38916070663b766df1b38cf881d
Oleg Pimenov <pimenov.o@gmail.com>
2024-12-10 14:32:03 +0300

    chore: fixed exports

--- packages/primitives/avatar/index.ts
+++ packages/primitives/avatar/index.ts
@@ -1,6 +1,5 @@
 export * from './src/avatar-fallback.directive';
-export type { RdxAvatarFallbackProps } from './src/avatar-fallback.directive';
 export * from './src/avatar-image.directive';
-export type { RdxAvatarImageProps } from './src/avatar-image.directive';
 export * from './src/avatar-root.directive';
+export type { RdxImageLoadingStatus } from './src/avatar-root.directive';
 export * from './src/avatar.config';
