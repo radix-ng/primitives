bed3ab11badbebf33de539ddc2b618daffd19438
Oleg Pimenov <pimenov.o@gmail.com>
2024-12-18 19:11:11 +0300

    feat(switch): added inputId and aria labels for screenreader

--- packages/primitives/switch/src/switch-input.directive.ts
+++ packages/primitives/switch/src/switch-input.directive.ts
@@ -9,17 +9,27 @@ import { injectSwitch } from './switch-root.directive';
         type: 'checkbox',
         role: 'switch',
         tabindex: '-1',
+        '[attr.id]': 'switchRoot.inputId()',
         '[attr.defaultChecked]': 'switchRoot.checkedState()',
         '[attr.aria-checked]': 'switchRoot.checkedState()',
         '[attr.aria-hidden]': 'true',
+        '[attr.aria-label]': 'switchRoot.ariaLabel()',
+        '[attr.aria-labelledby]': 'switchRoot.ariaLabelledBy()',
         '[attr.aria-required]': 'switchRoot.required()',
         '[attr.data-state]': 'switchRoot.checkedState() ? "checked" : "unchecked"',
         '[attr.data-disabled]': 'switchRoot.disabledState() ? "true" : null',
         '[attr.disabled]': 'switchRoot.disabledState() ? switchRoot.disabledState() : null',
         '[attr.value]': 'switchRoot.checkedState() ? "on" : "off"',
-        style: 'transform: translateX(-100%); position: absolute; overflow: hidden; pointerEvents: none; opacity: 0; margin: 0;'
+        style: 'transform: translateX(-100%); position: absolute; overflow: hidden; pointerEvents: none; opacity: 0; margin: 0;',
+
+        '(blur)': 'onBlur()'
     }
 })
 export class RdxSwitchInputDirective {
     protected readonly switchRoot = injectSwitch();
+
+    /** @ignore */
+    protected onBlur() {
+        this.switchRoot.onTouched?.();
+    }
 }
