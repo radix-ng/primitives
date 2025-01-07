e006e8b91440142370fc8100af43c15da0f3fd13
Oleg Pimenov <pimenov.o@gmail.com>
2025-01-07 22:14:15 +0300

    chore(radix-docs): added Toggle

---
+++ apps/radix-docs/src/demos/primitives/toggle/toggle-demo.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { RdxToggleDirective } from '@radix-ng/primitives/toggle';
+import { Italic, LucideAngularModule } from 'lucide-angular';
+
+@Component({
+    selector: 'primitive-toggle-demo',
+    standalone: true,
+    imports: [
+        RdxToggleDirective,
+        LucideAngularModule
+    ],
+    template: `
+        <button class="Toggle" rdxToggle aria-label="Toggle italic">
+            <lucide-angular [img]="Italic" size="16" />
+        </button>
+    `,
+    styleUrl: 'toggle-demo.css'
+})
+export class ToggleDemoComponent {
+    protected readonly Italic = Italic;
+}
+
+export default ToggleDemoComponent;
