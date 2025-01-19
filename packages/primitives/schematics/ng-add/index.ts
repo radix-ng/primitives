7eb6e114fa305c095773e4e1d8e6ef0d6c4f713c
Oleg Pimenov <pimenov.o@gmail.com>
2025-01-19 09:43:10 +0300

    chore(primitives): updated schematic ng-add

--- packages/primitives/schematics/ng-add/index.ts
+++ packages/primitives/schematics/ng-add/index.ts
@@ -1,34 +1,69 @@
 import { Rule, SchematicContext, SchematicsException, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-import { addPackageToPackageJson } from '@angular/cdk/schematics/ng-add/package-config';
-import { readWorkspace } from '@schematics/angular/utility';
-import { Schema } from './schema';
-
-const DEPENDENCY_VERSIONS = {
-    ANGULAR_CDK: '^19.0.0',
-    RADIX_NG: '^28.0.0'
-};
+import { addPackageJsonDependency, NodeDependency, NodeDependencyType } from '@schematics/angular/utility/dependencies';
+import * as pkgJson from '../../package.json';
+import { getPackageVersionFromPackageJson, PackageJson } from './package-config';
 
 /**
  * This is executed when `ng add @radix-ng/primitives` is run.
  * It installs all dependencies in the 'package.json'.
  */
-export default function (options: Schema): Rule {
-    return async (tree: Tree, context: SchematicContext) => {
-        const { project } = options;
+export function ngAdd(): Rule {
+    return (tree: Tree, context: SchematicContext) => {
+        const pkg = pkgJson as unknown as PackageJson;
 
-        if (project) {
-            const workspace = await readWorkspace(tree);
-            const projectWorkspace = workspace.projects.get(project);
+        context.logger.info(``);
+        context.logger.info(`Installing ${pkg.name} dependencies...`);
 
-            if (!projectWorkspace) {
-                throw new SchematicsException(`Unable to find project '${project}' in the workspace`);
-            }
+        const ngCoreVersionTag = getPackageVersionFromPackageJson(tree, '@angular/core');
+        context.logger.info(`@angular/core version ${ngCoreVersionTag}`);
+
+        if (!ngCoreVersionTag) {
+            throw new SchematicsException('@angular/core version not found');
         }
 
-        addPackageToPackageJson(tree, '@angular/cdk', DEPENDENCY_VERSIONS.ANGULAR_CDK);
-        addPackageToPackageJson(tree, '@radix-ng/primitives', DEPENDENCY_VERSIONS.RADIX_NG);
+        const projectDeps: NodeDependency[] = [
+            {
+                name: '@angular/animations',
+                type: NodeDependencyType.Default,
+                version: ngCoreVersionTag,
+                overwrite: false
+            },
+            { name: '@angular/common', type: NodeDependencyType.Default, version: ngCoreVersionTag, overwrite: false },
+            { name: '@angular/core', type: NodeDependencyType.Default, version: ngCoreVersionTag, overwrite: false }
+        ];
+
+        projectDeps.forEach((dep) => {
+            addPackageJsonDependency(tree, dep);
+            context.logger.info(`Added dependency: ${dep.name}@${dep.version}`);
+        });
+
+        const libraryDeps: NodeDependency[] = [
+            {
+                name: '@angular/cdk',
+                type: NodeDependencyType.Default,
+                version: pkg.peerDependencies['@angular/cdk'],
+                overwrite: false
+            }
+        ];
+
+        libraryDeps.forEach((dep) => {
+            addPackageJsonDependency(tree, dep);
+            context.logger.info(`Added dependency: ${dep.name}@${dep.version}`);
+        });
+
+        const library: NodeDependency = {
+            name: pkg.name,
+            type: NodeDependencyType.Default,
+            version: `~${pkg.version}`,
+            overwrite: true
+        };
+
+        addPackageJsonDependency(tree, library);
+        context.logger.info(`Installing ${library.name}@${library.version}`);
 
         context.addTask(new NodePackageInstallTask());
+
+        return tree;
     };
 }
