5983053417ca027fa3781fe6ccdd7446dbe0d9c3
Oleg Pimenov <pimenov.o@gmail.com>
2025-02-13 17:51:46 +0300

    chore(core): internal changes - rename helpers

--- packages/primitives/core/index.ts
+++ packages/primitives/core/index.ts
@@ -5,9 +5,9 @@ export * from './src/id-generator';
 export * from './src/inject-ng-control';
 export * from './src/is-client';
 export * from './src/is-inside-form';
-export * from './src/isNumber';
+export * from './src/is-nullish';
+export * from './src/is-number';
 export * from './src/kbd-constants';
-export * from './src/nullish';
 export * from './src/window';
 
 export * from './src/positioning/constants';
