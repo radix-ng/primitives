b6521daad3646c45dea2898074cc11ce7e6b830e
Oleg Pimenov <pimenov.o@gmail.com>
2025-04-06 16:55:56 +0300

    fix: eslint

--- packages/primitives/calendar/stories/calendar-default.component.ts
+++ packages/primitives/calendar/stories/calendar-default.component.ts
@@ -49,7 +49,7 @@ import { RdxCalendarRootDirective } from '../src/calendar-root.directive';
                                 fill-rule="evenodd"
                                 d="M8.842 3.135a.5.5 0 0 1 .023.707L5.435 7.5l3.43 3.658a.5.5 0 0 1-.73.684l-3.75-4a.5.5 0 0 1 0-.684l3.75-4a.5.5 0 0 1 .707-.023"
                                 clip-rule="evenodd"
-                            ></path>
+                            />
                         </svg>
                     </button>
                     <div
@@ -76,7 +76,7 @@ import { RdxCalendarRootDirective } from '../src/calendar-root.directive';
                                 fill-rule="evenodd"
                                 d="M6.158 3.135a.5.5 0 0 1 .707.023l3.75 4a.5.5 0 0 1 0 .684l-3.75 4a.5.5 0 1 1-.73-.684L9.566 7.5l-3.43-3.658a.5.5 0 0 1 .023-.707"
                                 clip-rule="evenodd"
-                            ></path>
+                            />
                         </svg>
                     </button>
                 </div>
