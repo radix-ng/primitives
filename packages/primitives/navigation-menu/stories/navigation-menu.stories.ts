e42713e82aad218e0d7a5db815598406a5e25931
Oleg Pimenov <pimenov.o@gmail.com>
2025-04-10 20:26:36 +0300

    chore(navigation-menu): updated CaretDown svg

--- packages/primitives/navigation-menu/stories/navigation-menu.stories.ts
+++ packages/primitives/navigation-menu/stories/navigation-menu.stories.ts
@@ -278,6 +278,7 @@ export default {
                         top: 1px;
                         transition: transform 250ms ease;
                     }
+
                     [data-state='open'] > .CaretDown {
                         transform: rotate(-180deg);
                     }
@@ -391,8 +392,21 @@ export const Default: Story = {
                     <li class="NavigationMenuItem" rdxNavigationMenuItem value="learn">
                         <button class="NavigationMenuTrigger" rdxNavigationMenuTrigger>
                             Learn
-                            <svg class="CaretDown" aria-hidden width="9" height="6" viewBox="0 0 9 6">
-                                <path d="M0.5 0.5L4.5 4.5L8.5 0.5" stroke="currentColor" fill="none" />
+                            <svg
+                                class="CaretDown"
+                                width="15"
+                                height="15"
+                                viewBox="0 0 15 15"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                                    fill="currentColor"
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                ></path>
                             </svg>
                         </button>
 
@@ -442,8 +456,21 @@ export const Default: Story = {
                     <li class="NavigationMenuItem" rdxNavigationMenuItem value="overview">
                         <button class="NavigationMenuTrigger" rdxNavigationMenuTrigger>
                             Overview
-                            <svg class="CaretDown" aria-hidden width="9" height="6" viewBox="0 0 9 6">
-                                <path d="M0.5 0.5L4.5 4.5L8.5 0.5" stroke="currentColor" fill="none" />
+                            <svg
+                                class="CaretDown"
+                                width="15"
+                                height="15"
+                                viewBox="0 0 15 15"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    d="M4.18179 6.18181C4.35753 6.00608 4.64245 6.00608 4.81819 6.18181L7.49999 8.86362L10.1818 6.18181C10.3575 6.00608 10.6424 6.00608 10.8182 6.18181C10.9939 6.35755 10.9939 6.64247 10.8182 6.81821L7.81819 9.81821C7.73379 9.9026 7.61934 9.95001 7.49999 9.95001C7.38064 9.95001 7.26618 9.9026 7.18179 9.81821L4.18179 6.81821C4.00605 6.64247 4.00605 6.35755 4.18179 6.18181Z"
+                                    fill="currentColor"
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                ></path>
                             </svg>
                         </button>
 
