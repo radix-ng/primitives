9761880175e3da09bb11f8a50f37df5b7c204404
Oleg Pimenov <pimenov.o@gmail.com>
2025-04-24 20:07:19 +0300

    chore: updated styles

--- packages/primitives/calendar/stories/calendar-default.component.ts
+++ packages/primitives/calendar/stories/calendar-default.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CalendarDate } from '@internationalized/date';
+import { ChevronLeft, ChevronRight, LucideAngularModule } from 'lucide-angular';
 import { RdxCalendarCellTriggerDirective } from '../src/calendar-cell-trigger.directive';
 import { RdxCalendarGridHeadDirective } from '../src/calendar-grid-head.directive';
 import { RdxCalendarGridDirective } from '../src/calendar-grid.directive';
@@ -19,84 +20,40 @@ import { RdxCalendarRootDirective } from '../src/calendar-root.directive';
         RdxCalendarCellTriggerDirective,
         RdxCalendarHeadingDirective,
         RdxCalendarNextDirective,
-        RdxCalendarPrevDirective
+        RdxCalendarPrevDirective,
+        LucideAngularModule
     ],
     styleUrl: 'calendar-default.style.css',
     template: `
-        <div style="max-width: 680px; width: 300px;">
-            <div
-                #root="rdxCalendarRoot"
-                [value]="date"
-                style="margin-top: 1.5rem;border-radius: 0.75rem;background-color: #ffffff;padding: 1rem;box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);border: 1px solid #e5e7eb;"
-                rdxCalendarRoot
-                fixedWeeks
-            >
-                <div rdxCalendarHeader style="display: flex;align-items: center;justify-content: space-between;">
+        <div class="wrapper">
+            <div class="calendar-root" #root="rdxCalendarRoot" [value]="date" rdxCalendarRoot fixedWeeks>
+                <div class="calendar-header" rdxCalendarHeader>
                     <button class="icon-button" rdxCalendarPrev>
-                        <svg
-                            class="iconify iconify--radix-icons h-4 w-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                            aria-hidden="true"
-                            role="img"
-                            style=""
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 15 15"
-                        >
-                            <path
-                                fill="currentColor"
-                                fill-rule="evenodd"
-                                d="M8.842 3.135a.5.5 0 0 1 .023.707L5.435 7.5l3.43 3.658a.5.5 0 0 1-.73.684l-3.75-4a.5.5 0 0 1 0-.684l3.75-4a.5.5 0 0 1 .707-.023"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
+                        <lucide-angular [img]="ChevronLeft" size="16" style="display: flex;" />
                     </button>
-                    <div
-                        #head="rdxCalendarHeading"
-                        rdxCalendarHeading
-                        style="font-size: 0.875rem;color: #000000;font-weight: 500;"
-                    >
+                    <div class="calendar-heading" #head="rdxCalendarHeading" rdxCalendarHeading>
                         {{ head.headingValue() }}
                     </div>
                     <button class="icon-button" rdxCalendarNext>
-                        <svg
-                            class="iconify iconify--radix-icons h-4 w-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlns:xlink="http://www.w3.org/1999/xlink"
-                            aria-hidden="true"
-                            role="img"
-                            style=""
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 15 15"
-                        >
-                            <path
-                                fill="currentColor"
-                                fill-rule="evenodd"
-                                d="M6.158 3.135a.5.5 0 0 1 .707.023l3.75 4a.5.5 0 0 1 0 .684l-3.75 4a.5.5 0 1 1-.73-.684L9.566 7.5l-3.43-3.658a.5.5 0 0 1 .023-.707"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
+                        <lucide-angular [img]="ChevronRight" size="16" style="display: flex;" />
                     </button>
                 </div>
-                <div class="CalendarContainer">
-                    <table rdxCalendarGrid style="width: 100%; border-collapse: collapse; user-select: none;">
+
+                <div class="calendar-container">
+                    <table class="calendar-grid" rdxCalendarGrid>
                         @for (month of root.months(); track month) {
                             <thead rdxCalendarGridHead>
-                                <tr
-                                    style="display: grid;width: 100%;grid-template-columns: repeat(7,minmax(0,1fr)); margin-bottom: .25rem;"
-                                >
+                                <tr class="calendar-grid-head-row">
                                     @for (day of root.weekDays(); track $index) {
-                                        <th class="CalendarHeadCell">{{ day }}</th>
+                                        <th class="calendar-head-cell">{{ day }}</th>
                                     }
                                 </tr>
                             </thead>
-                            <tbody rdxCalendarGridBody style="display: grid;">
+                            <tbody class="calendar-grid-body" rdxCalendarGridBody>
                                 @for (weekDates of month.weeks; track $index) {
-                                    <tr style="display: grid; grid-template-columns: repeat(7, 1fr);">
+                                    <tr class="calendar-week-row">
                                         @for (weekDate of weekDates; track $index) {
-                                            <td style="position:relative; text-align: center; font-size: 0.8rem;">
+                                            <td class="calendar-cell-wrapper">
                                                 <div
                                                     class="calendar-day"
                                                     #cell="rdxCalendarCellTrigger"
@@ -120,4 +77,6 @@ import { RdxCalendarRootDirective } from '../src/calendar-root.directive';
 })
 export class CalendarDefaultComponent {
     readonly date = new CalendarDate(2024, 10, 3);
+    protected readonly ChevronLeft = ChevronLeft;
+    protected readonly ChevronRight = ChevronRight;
 }
