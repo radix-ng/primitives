e6bc99c4a5f4b95efb74cc2f60c8bdc1b3a98b96
Oleg Pimenov <pimenov.o@gmail.com>
2025-05-04 12:52:22 +0300

    fix: storybook markdown tables and exports for Number and Date fields

--- packages/primitives/date-field/index.ts
+++ packages/primitives/date-field/index.ts
@@ -1,2 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RdxDateFieldInputDirective } from './src/date-field-input.directive';
+import { RdxDateFieldRootDirective } from './src/date-field-root.directive';
+
+export * from './src/date-field-context.token';
 export * from './src/date-field-input.directive';
 export * from './src/date-field-root.directive';
+
+const _imports = [RdxDateFieldRootDirective, RdxDateFieldInputDirective];
+
+@NgModule({
+    imports: [..._imports],
+    exports: [..._imports]
+})
+export class RdxDateFieldModule {}
--- packages/primitives/number-field/index.ts
+++ packages/primitives/number-field/index.ts
@@ -1,2 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RdxNumberFieldDecrementDirective } from './src/number-field-decrement.directive';
+import { RdxNumberFieldIncrementDirective } from './src/number-field-increment.directive';
+import { RdxNumberFieldInputDirective } from './src/number-field-input.directive';
+import { RdxNumberFieldRootDirective } from './src/number-field-root.directive';
+
+export * from './src/number-field-context.token';
+export * from './src/number-field-decrement.directive';
+export * from './src/number-field-increment.directive';
 export * from './src/number-field-input.directive';
 export * from './src/number-field-root.directive';
+export * from './src/types';
+export * from './src/utils';
+
+const _imports = [
+    RdxNumberFieldRootDirective,
+    RdxNumberFieldInputDirective,
+    RdxNumberFieldIncrementDirective,
+    RdxNumberFieldDecrementDirective
+];
+
+@NgModule({
+    imports: [..._imports],
+    exports: [..._imports]
+})
+export class RdxNumberFieldModule {}
