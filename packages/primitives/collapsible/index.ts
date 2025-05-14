dddbed98b05ea20c5f62d12f8e518c75d37b554e
Oleg Pimenov <pimenov.o@gmail.com>
2025-05-14 22:35:36 +0300

    chore: upd exports

--- packages/primitives/collapsible/index.ts
+++ packages/primitives/collapsible/index.ts
@@ -1,3 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RdxCollapsibleContentPresenceDirective } from './src/collapsible-content-presence.directive';
+import { RdxCollapsibleContentDirective } from './src/collapsible-content.directive';
+import { RdxCollapsibleRootDirective } from './src/collapsible-root.directive';
+import { RdxCollapsibleTriggerDirective } from './src/collapsible-trigger.directive';
+
+export * from './src/collapsible-content-presence.directive';
 export * from './src/collapsible-content.directive';
 export * from './src/collapsible-root.directive';
 export * from './src/collapsible-trigger.directive';
+
+const _imports = [
+    RdxCollapsibleContentDirective,
+    RdxCollapsibleRootDirective,
+    RdxCollapsibleTriggerDirective,
+    RdxCollapsibleContentPresenceDirective
+];
+
+@NgModule({
+    imports: [..._imports],
+    exports: [..._imports]
+})
+export class RdxCollapsibleModule {}
