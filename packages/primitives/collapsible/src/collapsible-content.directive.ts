effe641f05c3fe71d02f1db9ea3855656e3a843c
Oleg Pimenov <pimenov.o@gmail.com>
2025-05-15 22:58:24 +0300

    chore: animaiton

--- packages/primitives/collapsible/src/collapsible-content.directive.ts
+++ packages/primitives/collapsible/src/collapsible-content.directive.ts
@@ -1,4 +1,5 @@
-import { afterNextRender, computed, Directive, ElementRef, inject } from '@angular/core';
+import { computed, Directive, ElementRef, inject, signal } from '@angular/core';
+import { watch } from '@radix-ng/primitives/core';
 import { injectCollapsibleRootContext } from './collapsible-root.directive';
 
 @Directive({
@@ -8,8 +9,11 @@ import { injectCollapsibleRootContext } from './collapsible-root.directive';
 
         '[attr.data-state]': 'rootContext.open() ? "open" : "closed"',
         '[attr.data-disabled]': 'rootContext.disabled() ? "true" : undefined',
-        '[attr.hidden]': '!rootContext.open() || null',
-        '[style]': 'hostStyles()'
+        '[style.display]': "!rootContext.open() ? 'none' : undefined",
+        '[style.--radix-collapsible-content-width.px]': 'width()',
+        '[style.--radix-collapsible-content-height.px]': 'height()',
+        '(animationstart)': 'onAnimationStart()',
+        '(animationend)': 'onAnimationEnd()'
     }
 })
 export class RdxCollapsibleContentDirective {
@@ -17,44 +21,58 @@ export class RdxCollapsibleContentDirective {
 
     protected readonly rootContext = injectCollapsibleRootContext()!;
 
-    protected hostStyles = computed(() => ({
-        '--radix-collapsible-content-height': `${this.dimensions().height}px`,
-        '--radix-collapsible-content-width': `${this.dimensions().width}px`
-    }));
+    readonly isOpen = computed(() => this.rootContext.open());
 
-    private dimensions = computed(() => {
-        this.rootContext.open();
+    readonly height = signal(0);
+    readonly width = signal(0);
+    readonly isMountAnimationPrevented = signal(true);
 
-        this.elementRef.nativeElement.style.transitionDuration = '0s';
-        this.elementRef.nativeElement.style.animationName = 'none';
-
-        const rect = this.elementRef.nativeElement.getBoundingClientRect();
-
-        if (this.wasMountAnimationPrevented) {
-            this.elementRef.nativeElement.style.transitionDuration = this.originalStyles.transitionDuration;
-            this.elementRef.nativeElement.style.animationName = this.originalStyles.animationName;
-        }
-
-        return rect;
-    });
-
-    private originalStyles!: {
-        transitionDuration: string;
-        animationName: string;
+    private originalStyles: { transition: string; animation: string } = {
+        transition: '',
+        animation: ''
     };
 
-    private wasMountAnimationPrevented = false;
-
     constructor() {
-        afterNextRender(() => {
-            this.originalStyles = {
-                transitionDuration: this.elementRef.nativeElement.style.transitionDuration,
-                animationName: this.elementRef.nativeElement.style.animationName
-            };
-
+        watch([this.isOpen], ([value]) => {
             requestAnimationFrame(() => {
-                this.wasMountAnimationPrevented = true;
+                const node = this.elementRef.nativeElement;
+                if (!node) return;
+
+                if (this.isMountAnimationPrevented()) {
+                    this.originalStyles = {
+                        transition: node.style.transition,
+                        animation: node.style.animation
+                    };
+                }
+
+                node.style.transition = 'none';
+                node.style.animation = 'none';
+
+                node.getBoundingClientRect();
+
+                const rect = node.getBoundingClientRect();
+                this.height.set(rect.height);
+                this.width.set(rect.width);
+
+                if (!this.isMountAnimationPrevented()) {
+                    node.style.transition = this.originalStyles.transition;
+                    node.style.animation = this.originalStyles.animation;
+                }
             });
         });
+    }
+
+    ngOnInit() {
+        setTimeout(() => {
+            this.isMountAnimationPrevented.set(false);
+        });
+    }
+
+    onAnimationStart() {
+        console.log('animation start');
+    }
+
+    onAnimationEnd() {
+        console.log('animation end');
     }
 }
