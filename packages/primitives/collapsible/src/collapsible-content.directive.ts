f8b282bdf9fa5e1a926a2c5f5bdd79cc92691192
Oleg Pimenov <pimenov.o@gmail.com>
2025-05-16 09:09:20 +0300

    chore: upd animation

--- packages/primitives/collapsible/src/collapsible-content.directive.ts
+++ packages/primitives/collapsible/src/collapsible-content.directive.ts
@@ -1,4 +1,4 @@
-import { computed, Directive, ElementRef, inject, OnInit, signal } from '@angular/core';
+import { afterNextRender, computed, Directive, ElementRef, inject, signal } from '@angular/core';
 import { watch } from '@radix-ng/primitives/core';
 import { injectCollapsibleRootContext } from './collapsible-root.directive';
 
@@ -10,7 +10,6 @@ import { injectCollapsibleRootContext } from './collapsible-root.directive';
         '[attr.data-state]': 'rootContext.open() ? "open" : "closed"',
         '[attr.data-disabled]': 'rootContext.disabled() ? "true" : undefined',
         '[style.display]': 'hiddenSignal() ? "none" : undefined',
-        '[style.overflow]': '"hidden"',
         //  '[style.display]': "!rootContext.open() ? 'none' : undefined",
         '[style.--radix-collapsible-content-width.px]': 'width()',
         '[style.--radix-collapsible-content-height.px]': 'height()',
@@ -18,7 +17,7 @@ import { injectCollapsibleRootContext } from './collapsible-root.directive';
         '(animationend)': 'onAnimationEnd()'
     }
 })
-export class RdxCollapsibleContentDirective implements OnInit {
+export class RdxCollapsibleContentDirective {
     private readonly elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
 
     protected readonly rootContext = injectCollapsibleRootContext()!;
@@ -42,16 +41,9 @@ export class RdxCollapsibleContentDirective implements OnInit {
             if (isOpen) {
                 this.hiddenSignal.set(false);
 
-                requestAnimationFrame(() => {
+                setTimeout(() => {
                     const node = this.elementRef.nativeElement;
                     if (!node) return;
-
-                    if (this.isMountAnimationPrevented()) {
-                        this.originalStyles = {
-                            transition: node.style.transition,
-                            animation: node.style.animation
-                        };
-                    }
 
                     node.style.transition = 'none';
                     node.style.animation = 'none';
@@ -67,15 +59,22 @@ export class RdxCollapsibleContentDirective implements OnInit {
                         node.style.animation = this.originalStyles.animation;
                     }
 
-                    this.isMountAnimationPrevented.set(false);
                     console.log('requestAnimationFrame end');
                 });
             }
         });
-    }
 
-    ngOnInit() {
-        this.isMountAnimationPrevented.set(false);
+        afterNextRender(() => {
+            this.originalStyles = {
+                transition: this.elementRef.nativeElement.style.transition,
+                animation: this.elementRef.nativeElement.style.animation
+            };
+            requestAnimationFrame(() => {
+                this.isMountAnimationPrevented.set(false);
+            });
+
+            console.log('afterNextRender end');
+        });
     }
 
     onAnimationStart() {
