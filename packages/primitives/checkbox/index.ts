ac998da1cba09bad0bfdaa3d4463a63b2fcd6053
Oleg Pimenov <pimenov.o@gmail.com>
2025-09-25 21:48:57 +0300

    chore: upd barrel

--- packages/primitives/checkbox/index.ts
+++ packages/primitives/checkbox/index.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RdxCheckboxButtonDirective } from './src/checkbox-button';
 import { RdxCheckboxIndicatorDirective } from './src/checkbox-indicator';
+import { RdxCheckboxIndicatorPresenceDirective } from './src/checkbox-indicator-presence';
 import { RdxCheckboxInputDirective } from './src/checkbox-input';
 import { RdxCheckboxRootDirective } from './src/checkbox-root';
 
 export * from './src/checkbox-button';
 export * from './src/checkbox-indicator';
+export * from './src/checkbox-indicator-presence';
 export * from './src/checkbox-input';
 export * from './src/checkbox-root';
 export type { CheckedState } from './src/checkbox-root';
@@ -14,7 +16,8 @@ export const checkboxImports = [
     RdxCheckboxInputDirective,
     RdxCheckboxRootDirective,
     RdxCheckboxButtonDirective,
-    RdxCheckboxIndicatorDirective
+    RdxCheckboxIndicatorDirective,
+    RdxCheckboxIndicatorPresenceDirective
 ];
 
 @NgModule({
