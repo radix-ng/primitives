24446f9362a2ad37b3af0ace160944c933668dd6
Oleg Pimenov <pimenov.o@gmail.com>
2025-10-03 09:40:37 +0300

    feat(radix-storybook): migrated to vite (#365)

--- apps/radix-storybook/.storybook/main.ts
+++ apps/radix-storybook/.storybook/main.ts
@@ -1,7 +1,9 @@
-import type { StorybookConfig } from '@storybook/angular';
+import type { StorybookConfig } from '@analogjs/storybook-angular';
 import { createRequire } from 'node:module';
 import { dirname, join } from 'node:path';
 import remarkGfm from 'remark-gfm';
+import { mergeConfig, UserConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const require = createRequire(import.meta.url);
 
@@ -9,7 +11,7 @@ const config: StorybookConfig = {
     stories: [
         '../docs/**/*.docs.mdx',
         '../../../packages/primitives/**/*.docs.mdx',
-        '../../../packages/primitives/**/*.stories.@(js|ts)'
+        '../../../packages/primitives/**/*.stories.ts'
     ],
 
     addons: [
@@ -27,12 +29,18 @@ const config: StorybookConfig = {
     ],
 
     framework: {
-        name: getAbsolutePath('@storybook/angular'),
+        name: '@analogjs/storybook-angular',
         options: {}
     },
 
     core: {
         disableTelemetry: false
+    },
+
+    async viteFinal(config: UserConfig) {
+        return mergeConfig(config, {
+            plugins: [viteTsConfigPaths()]
+        });
     },
 
     docs: {}
