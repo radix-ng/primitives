de741670febde1879a1948454e903235d81d1274
Artem Belik <artyombelyk@gmail.com>
2026-04-05 13:37:25 +0300

    refactor: bump eslint to 9

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    '*.{css,scss,md,mdx,yml,json,js,cjs,mjs,html,ts,astro}': 'prettier --write',
+    '*': 'prettier --write --ignore-unknown',
     '*.{css,scss}': 'stylelint --max-warnings=0 --fix',
     '*.{js,cjs,mjs,ts,html}': 'eslint --max-warnings=0 --fix'
 };
