3ac553ce190c885141f6ea5dfd2c149a313e840e
Oleg Pimenov <pimenov.o@gmail.com>
2026-06-19 12:15:31 +0300

    docs: show package version on landing

--- apps/radix-storybook/.storybook/manager.tsx
+++ apps/radix-storybook/.storybook/manager.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
-import { addons } from 'storybook/manager-api';
+import { addons, types } from 'storybook/manager-api';
+import packageJson from '../../../packages/primitives/package.json';
 import { installRdxDirectiveHighlight } from './highlight-rdx-directives';
 import './rdx-directive-highlight.css';
 import { light } from './themes';
 
 installRdxDirectiveHighlight();
 
+const packageVersionUrl = `https://www.npmjs.com/package/@radix-ng/primitives/v/${packageJson.version}`;
+
 // Component-level titles of primitives that are still in beta.
 const BETA = new Set(['Alert Dialog', 'Select']);
+
+function PackageVersionTool() {
+    return React.createElement(
+        'a',
+        {
+            href: packageVersionUrl,
+            target: '_blank',
+            rel: 'noreferrer',
+            title: `@radix-ng/primitives ${packageJson.version}`,
+            'aria-label': `Open @radix-ng/primitives ${packageJson.version} on npm`,
+            style: {
+                display: 'inline-flex',
+                alignItems: 'center',
+                gap: '6px',
+                height: '28px',
+                padding: '0 8px',
+                color: 'inherit',
+                textDecoration: 'none',
+                fontSize: '12px',
+                lineHeight: 1,
+                whiteSpace: 'nowrap'
+            }
+        },
+        React.createElement(
+            'svg',
+            { width: 14, height: 14, viewBox: '0 0 24 24', fill: 'currentColor', 'aria-hidden': true },
+            React.createElement('path', { d: 'M3 3h18v18H3V3Zm3.5 14h3V8.5H12V17h3V8.5h2.5V17h3V6H6.5v11Z' })
+        ),
+        React.createElement('span', null, 'npm'),
+        React.createElement(
+            'span',
+            { style: { opacity: 0.68, fontFamily: 'JetBrains Mono, monospace' } },
+            packageJson.version
+        )
+    );
+}
+
+addons.register('radix-ng/package-version', () => {
+    addons.add('radix-ng/package-version/tool', {
+        title: 'Package version',
+        type: types.TOOLEXTRA,
+        render: PackageVersionTool
+    });
+});
 
 addons.setConfig({
     theme: light,
